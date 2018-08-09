7d1013ff3d46c807ff042d8366a325ce05c0e543
jpezoldt <joern_pezoldt@yahoo.de>
2018-08-09 11:11:26 +0200

    No change

--- UPDE/ATAC/ATAC_FSC/15_ATAC_Sort_Thresh_MACS2.R
+++ UPDE/ATAC/ATAC_FSC/15_ATAC_Sort_Thresh_MACS2.R
@@ -7,14 +7,21 @@
 
 #Libraries
 library("GenomicRanges")
+library("data.table")
+library("stringr")
+
+#####
+#Global variables
+#####
 
 # Input required: Set path to directory with the MACS2 XLS files
-path_input <- setwd("/home/pezoldt/NAS2/pezoldt/Analysis/ATACseq/ATAC_FSC_all/peaks/broad/MACS2_broad_XLS")
-path_input_blacklist <- "/home/pezoldt/NAS2/pezoldt/Analysis/ATACseq/ATAC_FSC_all/peaks/broad/mm10.blacklist_kundaje_20180718_final.bed"
-path_output <- "/home/pezoldt/NAS2/pezoldt/Analysis/ATACseq/ATAC_FSC_all/peaks/broad/MACS2_broad/Run_3"
+path_input <- setwd("/home/pezoldt/NAS2/pezoldt/Analysis/ATACseq/ATAC_FSC_all/peaks/broad/MACS2_XLS")
+path_input_blacklist <- "/home/pezoldt/NAS2/pezoldt/Data/ATACseq/mm10.blacklist_kundaje_20180718_final.bed"
+path_output <- "/home/pezoldt/NAS2/pezoldt/Analysis/ATACseq/ATAC_FSC_all/peaks/broad/MACS2_broad/Run_4_in_all"
+path_output_at_least_2 <- "/home/pezoldt/NAS2/pezoldt/Analysis/ATACseq/ATAC_FSC_all/peaks/broad/MACS2_broad/Run_4_min2tracks"
+
 # Input required: add group names that allow to distinguish experimental conditions across replicates
 experimental_groups <- c("ATAC_mLNGF","ATAC_mLNSPF","ATAC_pLNGF","ATAC_pLNSPF")
-
 
 #####
 #1) Filter MACS Output
@@ -89,7 +96,7 @@ for(i in seq(length(MACS2_thresh_peaks.list))){
 names(MACS2_thresh_peaks_minus_bl.list) <- paste("Thresh_bl_",list.files(pattern="*.xls"),sep="")
 
 #####
-#3) Identify Peaks present in all files
+#3) Identify Peaks present in all files in all tracks
 #####
 #for every experimental group
 #grab replicates
@@ -140,7 +147,7 @@ for(i in seq(length(experimental_groups))){
     #make Granges object of 2nd in list
     gr_index <- with(grange_obj_list_j[[index]], GRanges(seqnames = seqnames, IRanges(start = start, end = end)))
     #find overlap
-    overlap_index <- suppressWarnings(findOverlaps(gr_final, gr_index, minoverlap = 25))
+    overlap_index <- suppressWarnings(findOverlaps(gr_final, gr_index, minoverlap = 1))
     #get index numbers for overlapping peaks
     query_overlap <- queryHits(overlap_index)
     #get Granges object for overlapping peaks
@@ -162,6 +169,94 @@ for(i in seq(length(experimental_groups))){
   write.table(merged_table_i, paste(path_output,"/", paste("Over_Merged_",experimental_groups[i],".bed",sep=""),sep=""), quote = F, sep="\t", row.names = F)
 }
 
-#Take output and pipe it into "JP_atac_merge_peaks_enrichment.bash"
+#Take output and pipe it into "20_..."
 
+#####
+#3) Identify Overlapping and non-overlapping Peaks
+#####
+#for every experimental group
+#grab replicates
+#make grange objects
+#find inner overlap looping over replicates
+#compile in one file
 
+#Initiate empty list to store grange object per group
+grange_common_group <- list()
+for(i in seq(length(experimental_groups))){
+  
+  #for each experimental group
+  group_i <- experimental_groups[i]
+  #obtain the threshed MACS2_peak list names
+  group_names_i <- names(MACS2_thresh_peaks_minus_bl.list)[str_detect(names(MACS2_thresh_peaks_minus_bl.list),group_i)]
+  #grab the respective table
+  group_table_i <- MACS2_thresh_peaks_minus_bl.list[group_names_i]
+  
+  #Intiate empty list to store grange objects per replicate within experimental group
+  grange_obj_list_j <- list()
+  
+  
+  for(j in seq(length(group_table_i))){
+    #make grange object for each replicate
+    grange_obj_j <- group_table_i[[j]]
+    
+    #store grange in experimental group list
+    grange_obj_list_j[[j]] <- grange_obj_j
+    
+  }
+  #name replicates
+  names(grange_obj_list_j) <- paste("Grange_",group_names_i,sep="")
+  #check if replicates correspond to input
+  print(length(grange_obj_list_j))
+  print(names(grange_obj_list_j))
+  
+  #Find common peak regions across replicates per experimental group
+  #grange_common_group[[i]] <- Reduce(subsetByOverlaps, grange_obj_list_j)
+  
+  #loop over Granges List object and iteratively identify the overlap
+  #Generate Granges object to start loop
+  gr_final <- with(grange_obj_list_j[[1]], GRanges(seqnames = seqnames, IRanges(start = start, end = end)))
+  print(length(gr_final))
+  for(k in seq(length(grange_obj_list_j)-1)){
+    #add plus one to start with the second object
+    index = k + 1
+    print(index)
+    #make Granges object of 2nd in list
+    gr_index <- with(grange_obj_list_j[[index]], GRanges(seqnames = seqnames, IRanges(start = start, end = end)))
+    #find overlap
+    overlap_index <- suppressWarnings(findOverlaps(gr_final, gr_index, minoverlap = 1))
+    #get index numbers for overlapping peaks
+    query_overlap <- queryHits(overlap_index)
+    #get Granges object for overlapping peaks
+    gr_final <- gr_final[query_overlap]
+    #non-overlapping peaks
+    gr_nonoverlap <- suppressWarnings(setdiff(gr_final, gr_index))
+    #appending overlapping and nonoverlapping regions
+    df_final <- as.data.frame(gr_final)
+    df_nonoverlap <- as.data.frame(gr_nonoverlap)
+    df_final <- rbind(df_final, df_nonoverlap)
+    #Make granges object
+    gr_final <- makeGRangesFromDataFrame(df_final,
+                                             seqnames.field=c("seqnames", "seqname",
+                                                              "chromosome", "chrom",
+                                                              "chr", "chromosome_name",
+                                                              "seqid"),
+                                             start.field="start",
+                                             end.field=c("end"),
+                                             strand.field="strand",
+                                             starts.in.df.are.0based=FALSE)
+    print(length(gr_final))
+  }
+  grange_common_group[[i]] <- gr_final
+}
+
+#Make .bed from grange object
+for(i in seq(length(experimental_groups))){
+  print(experimental_groups[i])
+  print(i)
+  merged_table_i <- as.data.frame(grange_common_group[[i]])
+  merged_table_i <- cbind(merged_table_i[,1:3],rep(paste("Over_Merged_",experimental_groups[i],sep=""),nrow(merged_table_i)))
+  print(head(merged_table_i))
+  names(merged_table_i) <- NULL
+  #rownames(merged_table_i) <- NULL
+  write.table(merged_table_i, paste(path_output_at_least_2,"/", paste("Over_Merged_",experimental_groups[i],".bed",sep=""),sep=""), quote = F, sep="\t", row.names = F)
+}
